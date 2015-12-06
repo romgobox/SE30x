88a5df371191e53a2e0826850964cc295eaf0a16
romgobox <romgobox@gmail.com>
2015-12-06 18:35:38 +0300

    Добавлен метод для записи зафиксированных показаний в БД

--- meter.py
+++ meter.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import time
 from datetime import datetime
+import sqlite3
 
 
 from protocol import SE30X
@@ -26,6 +27,8 @@ conn = {
 }
 '''
 
+DATABASE = 'se.db'
+
 class Meter(object):
         """Класс прибора учета"""
 
@@ -43,3 +46,19 @@ class Meter(object):
 
         def __repr__(self):
             return '<meter adr: %s, number: %s>' % (str(self.adr), self.number)
+
+        def saveFixDayValues(self):
+            for date, value in self.fixDayValue.items():
+                if value:
+                    dateP = datetime.strptime(date, '%d.%m.%y')
+                    dateval = datetime.strftime(dateP, '%d.%m.%Y %H:%M:%S')
+                    now = datetime.now()
+                    datercv = datetime.strftime(now, '%d.%m.%Y %H:%M:%S')
+                    channels_sql = '''
+                    INSERT INTO meter_values VALUES(Null, {id}, 1, '{datercv}', '{dateval}', {value})
+                    '''.format(id=self.id, datercv=datercv, dateval=dateval, value=value['Sum'])
+                    con = sqlite3.connect(DATABASE)
+                    cur = con.cursor()
+                    import pudb; pu.db
+                    channelsDB = cur.execute(channels_sql)
+                    con.commit()
